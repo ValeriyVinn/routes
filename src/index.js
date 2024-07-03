88cb9dda67aee7dc604486d301f5e04444981fb1
ValeriyVinn <valeriy.all@gmail.com>
2024-07-03 20:01:44 +0300

    Started Lo-project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { AppLo } from 'components/AppLo';
+// import { App } from 'components/App';
+
 import { BrowserRouter } from 'react-router-dom';
 // import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/routes">
-      <App />
+      <AppLo />
+      {/* <App /> */}
+
     </BrowserRouter>
   </React.StrictMode>
 );
